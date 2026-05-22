b6aa382cc6dfcb471868f629095a999cd88a3685
jiang-zhexin <162887873+jiang-zhexin@users.noreply.github.com>
2026-05-22 13:03:01 +0800

    feat: add ci

--- inbound.test.ts
+++ inbound.test.ts
@@ -8,8 +8,7 @@ createInbounds([
         listen_port: 80,
         password: '',
         method: '2022-blake3-aes-128-gcm',
-        // This error is used to check type safety
-        detour: '',
+        detour: 'ccccc',
     },
     {
         type: 'anytls',
